a65dbada2a7a49626a6596dd4ec7154d31ce71d8
Ethan Sitnek <edjsitnek@gmail.com>
2025-06-16 22:40:51 -0400

    add basic pages

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,5 @@
+export default function Contact() {
+  return (
+    <h1>Ethan Sitnek's Contact Info</h1>
+  );
+}
---
+++ src/app/projects/page.jsx
@@ -0,0 +1,5 @@
+export default function Projects() {
+  return (
+    <h1>Ethan Sitnek's Projects</h1>
+  );
+}
